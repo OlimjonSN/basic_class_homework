d0551d7e26f829e1592dcfac63ba29def1120a96
Yaxshiboyev Olimjon <63545453+OlimjonSN@users.noreply.github.com>
2023-02-20 15:19:19 +0500

    Update basic_class_homework06.dart

--- bin/basic_class_homework06.dart
+++ bin/basic_class_homework06.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_initializing_formals
 
 /*
-Create a "Person" class that has a name("name") and a age("age")
+Create a "Person" class that has a name("ali") and a age("25")
 */
 
 void main() {}
