98e1a2a8ef03cf248686c2c2c4b10e5b2bda46c7
Olimjon <olimjonsn230@gmail.com>
2023-02-20 15:25:18 +0500

    done

--- bin/basic_class_homework06.dart
+++ bin/basic_class_homework06.dart
@@ -1,7 +1,11 @@
 // ignore_for_file: prefer_initializing_formals
 
 /*
-Create a "Person" class that has a name("ali") and a age("25")
+Create a "Person" class that has a name("name") and a age("25")
 */
+class Person {
+  String name = "ali";
+  int age = 25;
+}
 
 void main() {}
